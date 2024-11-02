a3402deb1696c3bf7e621d4b1e281f76d7ef378e
Vikash Kumar <vikashtech0089@gmail.com>
2024-11-03 05:20:26 +0530

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
+// Render signup page
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Handle signup
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    res.redirect('/auth/login');
+});
+
+// Render login page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Handle login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+        req.session.user = user;
+        res.redirect('/');
+    } else {
+        res.redirect('/auth/login');
+    }
+});
+
+// Handle logout
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+module.exports = router;
